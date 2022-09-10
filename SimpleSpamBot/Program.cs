093ad2e96f732cdf2997aa30e521b2cdd0536f62
ED-LetsCode <eray.demirel@lets-code.at>
2022-09-10 20:21:20 +0200

    Projektdateien hinzufügen.

---
+++ SimpleSpamBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using WindowsInput;
+
+namespace SimpleSpamBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //start the program and click on the textbox in the application where you want to send the spam message
+            StartSpam(1000, "Enter here your'e message");
+        }
+
+        private static void StartSpam(int count, string spamMessage)
+        {
+            InputSimulator input = new InputSimulator();
+            Thread.Sleep(5000);
+
+            for (int i = 0; i < count; i++) //Enter here how many times
+            {
+                input.Keyboard.TextEntry(spamMessage);
+                input.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
