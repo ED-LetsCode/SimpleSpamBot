6a7dfa287703f1e34b2cb6c9f5dd8f1537b31be4
Eray Demirel <113133576+ED-LetsCode@users.noreply.github.com>
2022-09-10 20:25:30 +0200

    Update Program.cs

--- SimpleSpamBot/Program.cs
+++ SimpleSpamBot/Program.cs
@@ -7,7 +7,8 @@ namespace SimpleSpamBot
     {
         static void Main(string[] args)
         {
-            //Start the program and click on the textbox in the application where you want to send the spam message
+            //Start the program and click on the textbox in the application where you want to send the spam message. 
+            //After start you have 5 seconds until the spam is started.
             StartSpam(1000, "Enter here your'e message");
         }
 
